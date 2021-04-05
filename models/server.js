333e102dc424a3477b5f1d25bb276918ecc24d62
nk0dmk <web@ahunki.com>
2021-04-05 23:23:08 +0200

    hardcoded APIPATH constant

--- models/server.js
+++ models/server.js
@@ -11,7 +11,7 @@ class Server {
 
         this.app = express();
         this.port = process.env.PORT;
-        this.userApiPath = process.env.USERAPIPATH;
+        this.userApiPath = '/api/users';
 
         // Conectar DB
         this.connDb();
